45dcf839a025a04c3e09117ae16fd405b0851002
Teoh TeikToe <teohteiktoe@gmail.com>
2024-10-19 05:37:39 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 from flask import Flask
+from flask import render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/prediction_DBS",methods=["GET","POST"])
+def prediction_DBS():
+    return(render_template("prediction_DBS.html"))
+
+if __name__ == "__main__":
+    app.run()
+
